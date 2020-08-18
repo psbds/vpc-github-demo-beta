44902040539937485053b931a1631fce8b03d258
Paulo Sérgio Baima <padasil@microsoft.com>
2020-08-18 19:02:52 -0300

    Release/v0.1 (#11)
    * Add unit tests project (#3)
    
    * Feature/unit tests (#4)
    
    * Add unit tests project
    
    * testing issue closing
    
    * Workflow for Pull Requests unit tests (#5)
    
    * Create pr-tests.yml
    
    * pr tests
    
    * pr tests
    
    * Feature/workflow deployment (#7)
    
    * workflow-deployment
    
    * workflow-deployment
    
    * workflow-deployment
    
    * workflow-deployment
    
    * workflow-deployment
    
    * workflow-deployment
    
    * workflow-deployment
    
    * workflow-deployment
    
    * merge develop into release v0.1 (#10)
    
    * Feature/workflow deployment (#8)
    
    * workflow-deployment
    
    * workflow-deployment
    
    * workflow-deployment
    
    * workflow-deployment
    
    * workflow-deployment
    
    * workflow-deployment
    
    * workflow-deployment
    
    * workflow-deployment
    
    * adding staging workflow
    
    * Feature/workflow deployment (#9)
    
    * workflow-deployment
    
    * workflow-deployment
    
    * workflow-deployment
    
    * workflow-deployment
    
    * workflow-deployment
    
    * workflow-deployment
    
    * workflow-deployment
    
    * workflow-deployment
    
    * adding staging workflow
    
    * adding staging workflow

---
+++ Contoso.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Contoso.Tests
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+
+			// Dummy Tests
+			Assert.AreEqual("Answer to life the universe and everything".Length, 42);
+		
+		}
+	}
+}
